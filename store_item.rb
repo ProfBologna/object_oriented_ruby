254e5456d242c94ac7d1787cc11e2fdb6b3ea10e
ProfBologna <sam.aporia@gmail.com>
2017-08-21 20:52:25 -0500

    Initial exercise for items

---
+++ store_item.rb
@@ -0,0 +1,6 @@
+items_1 = ["item_name" => "chair", "item_cost" => 5, "color" => "brown", "item_status" => true]
+
+item_2 = [item_name: table, item_cost: 80000, color: red, item_status: true]
+
+item_3 = [:item_name => "blanket", :item_cost => 999, :color => "blue", :item_status => false]
+
